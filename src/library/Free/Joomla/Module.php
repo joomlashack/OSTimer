122b0fb04c76d8c19edfba35368f1a4dc387515c
Valentín <htmgarcia@gmail.com>
2017-06-19 15:51:44 -0500

    Remove log

--- src/library/Free/Joomla/Module.php
+++ src/library/Free/Joomla/Module.php
@@ -221,7 +221,6 @@ class Module extends AbstractFlexibleModule
 
                 if(document.getElementById("clockDayJS<?php echo($id);?>") && secs > 0) {
                     CountBackDays<?php echo($id);?>(secs);
-                    console.log(secs);
                 }
 
                 DisplayStr = DisplayFormat<?php echo($id);?>.replace(/%%D%%/g, calcage<?php echo($id);?>(secs, 86400, 100000));
